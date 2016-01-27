52a30e5525eb9dffa54f86ef06d41bd0aeb3af91
Evan Beneroff <ebenerof@calpoly.edu>
2016-01-27 15:02:34 -0800

    all the good stuff
    evan

---
+++ Dictionary.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+
+/// <summary>
+/// Dictionary class for Hangman.exe, can lookup word based on difficulty, and scan to see if an input letter is correct
+/// </summary>
+public class Dictionary
+{
+    private int num = 0;
+    private Random rnd;
+    private string word;
+    private FileStream file;
+    private StreamReader sr;
+    private string[] dict = new string[109546];
+
+    //Constructor
+	public Dictionary()
+	{
+        file = new FileStream(@"C:\Users\BenerE01\Desktop\hangman\Dictionary.txt", FileMode.Open, FileAccess.Read);
+        sr = new StreamReader(file);
+
+        for (int b = 0; b < 109546; b++)
+        {
+            dict[b] = sr.ReadLine();
+        }
+
+        word = sr.ReadLine();
+	}
+
+    //Find new word from dictionary.txt
+    //difficulty 1 = 4 letters, 2 = 5, 3 = 6, 4 = 7, 5 = 8
+    public void newWord(int difficulty)
+    {
+        rnd = new Random();
+        //num = rnd.Next(0, 109546);
+
+        while (word.Length != (difficulty + 3))
+        {
+            num = rnd.Next(0, 109546);
+            word = dict[num];
+        }
+    }
+
+    //Get method for returning interval variable word
+    public string getWord()
+    {
+        return word;
+    }
+}
