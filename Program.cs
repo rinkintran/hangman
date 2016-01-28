bc5f894dd2c93fb6df47ee037b19882fff0818f0
Evan Beneroff <ebenerof@calpoly.edu>
2016-01-28 10:32:40 -0800

    skelly++
    evan

--- Program.cs
+++ Program.cs
@@ -9,10 +9,11 @@ namespace hangman
     {
         static void Main(string[] args)
         {
+            int score = 0;
             Console.WriteLine("Would you like to play a game?");
             Console.WriteLine("How about Hangman?");
-            string skelly = "      _______\n     |       |\n     0       |\n    _|_      |\n     |       |\n    / \\      |\n             |\n         ____|____\n";
-            Console.WriteLine(skelly);
+            string hangman = "      _______\n     |       |\n     0       |\n    _|_      |\n     |       |\n    / \\      |\n             |\n         ____|____\n";
+            Console.WriteLine(hangman);
 
             Console.WriteLine("What difficulty would you like? (1-7)");
             int diff = Convert.ToInt32(Console.ReadLine());
@@ -21,6 +22,10 @@ namespace hangman
             d.newWord(diff);
             string word = d.getWord();
             Console.WriteLine(word);
+
+            Skelly skelly = new Skelly();
+            skelly.sorrySkelly(score);
+            Console.WriteLine(skelly.getSkelly());
             Console.ReadLine();
         }
     }
